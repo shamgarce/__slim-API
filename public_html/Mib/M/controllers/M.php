68d3886b2d44e6680043f375f9dac45df7c19a41
shampeak <ss012003>
2015-02-28 02:00:36 +0800

    1
    1

--- public_html/Mib/M/controllers/M.php
+++ public_html/Mib/M/controllers/M.php
@@ -15,12 +15,62 @@ class M extends CI_Controller
 	}
 
 
-	//主要的管理界面
-	public function index($ver = 0,$mm=0)
-	{
+    //主要的管理界面
+    public function index()
+    {
 
-		$this->load->view('M/M_index',$data);
-	}
+        $this->load->view('M/M_index',$data);
+    }
+
+    //设置界面
+    public function setup()
+    {
+        include 'Config/Set.php';
+        $ar = array();
+        for($i=1;$i<$num+1;$i++){
+           array_push($ar,$i);
+        }
+        array_push($ar,19);
+        array_push($ar,20);
+
+        $this->load->library('Fun');			//数据库
+        $ars = $this->fun->getstr($ar);
+        $sql = "select * from  doc_metro_group where groupid in($ars)";
+        $rc = $this->db->getall($sql);
+
+        $data['rc'] = $rc;
+        $data['num'] = $num;
+
+
+        $this->load->view('M/M_setup',$data);
+    }
+
+    //设置界面
+    public function setup_setgroupnum($num)
+    {
+        $num = intval($num);
+
+        $content = '<' . "?php\r\n" .
+                    '$num = ' . $num . ";\r\n".
+                    "\r\n?" . '>';
+        @file_put_contents('Config/Set.php', $content);
+
+        //19 有效
+        //其他有效
+        //============================================================
+        //缺少的加上
+        $sql = "update doc_metro_group set enable = 0 where groupid<>19";
+        $this->db->query($sql);
+        for($i=1;$i<$num+1;$i++){
+            $sql = "update doc_metro_group set enable = 1 where groupid = $i";
+            $this->db->query($sql);
+        }
+
+        $rs['code'] = 200;
+        $rs['msg'] = '提交成功';
+        echo json_encode($rs);
+    }
+
 
 
 }
--- public_html/Mib/Man/libraries/EasyCache/query_caches/sqlcache_config_file_530c2fadb1a1bbc5f810713195ef0853.php
+++ public_html/Mib/Man/libraries/EasyCache/query_caches/sqlcache_config_file_530c2fadb1a1bbc5f810713195ef0853.php
@@ -1,5 +1,5 @@
 <?php
-$this->mysql_config_cache_file_time = 1424877642.3934;
+$this->mysql_config_cache_file_time = 1425050698.5545;
 $this->timeline = 0;
 $this->timezone = 0;
 $this->platform = 'WINDOWS';
